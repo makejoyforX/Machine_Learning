54a7dc6e566f5346b2836fe8142a2ac8155e9651
Mondo <Turf1013@buaa.edu.cn>
2015-02-26 23:30:32 +0800

    finish base regression algorithnm

---
+++ Regression/regression.py
@@ -0,0 +1,203 @@
+from numpy import *
+import matplotlib.pyplot as plt
+
+def loadDataSet(fileName):
+	numFeat = len(open(fileName).readline().split('\t')) - 1
+	dataMat = []
+	labelMat = []
+	fr = open(fileName)
+	for line in fr.readlines():
+		lineArr = []
+		curLine = line.strip().split('\t')
+		for i in range(numFeat):
+			lineArr.append(float(curLine[i]))
+		dataMat.append(lineArr)
+		labelMat.append(float(curLine[-1]))
+	return dataMat, labelMat
+	
+def standRegress(xArr, yArr):
+	xMat = mat(xArr)
+	yMat = mat(yArr).T
+	xTx = xMat.T * xMat
+	if linalg.det(xTx) == 0.0:
+		print 'This matrix is singular, cannot do inverse'
+		return
+	ws = xTx.I * (xMat.T * yMat)
+	return ws
+	
+def testStandRegress():
+	xArr, yArr = loadDataSet('ex0.txt')
+	ws = standRegress(xArr, yArr)
+	print ws
+	xMat = mat(xArr)
+	yMat = mat(yArr)
+	fig = plt.figure()
+	ax = fig.add_subplot(111)
+	ax.scatter(
+		xMat[:,1].flatten().A[0], yMat.T[:, 0].flatten().A[0]
+	)
+	xCopy = xMat.copy()
+	xCopy.sort(0)
+	yHat = xCopy * ws
+	ax.plot(xCopy[:,1], yHat)
+	plt.show()
+	
+def lwlr(testPoint, xArr, yArr, k = 1.0):
+	xMat = mat(xArr)
+	yMat = mat(yArr).T
+	m = shape(xMat)[0]
+	weights = mat(eye(m))
+	for j in range(m):
+		# print 'testPoint =',testPoint
+		# print 'xArr[j,:] =',xMat[j,:]
+		diffMat = testPoint - xMat[j,:]
+		weights[j, j] = exp(diffMat*diffMat.T/(-2.0*k**2))
+	xTx = xMat.T * (weights * xMat)
+	if linalg.det(xTx) == 0.0:
+		print 'This matrix is singular, cannot do inverse'
+		return
+	ws = xTx.I * (xMat.T * weights * yMat)
+	return testPoint * ws
+	
+def lwlrTest(testArr, xArr, yArr, k = 1.0):
+	m = shape(testArr)[0]
+	yHat = zeros(m)
+	for i in range(m):
+		yHat[i] = lwlr(testArr[i], xArr, yArr, k)
+	return yHat
+	
+def testLwlr():
+	xArr, yArr = loadDataSet('ex0.txt')
+	yHat = lwlrTest(xArr, xArr, yArr, 0.003)
+	xMat = mat(xArr)
+	yMat = mat(yArr)
+	srtInd = xMat[:, 1].argsort(0)
+	xSort = xMat[srtInd][:,0,:]
+	# print 'xSort = ', xSort
+	# print 'xMat[srtInd] =', xMat[srtInd]
+	fig = plt.figure()
+	ax = fig.add_subplot(111)
+	ax.plot(xSort[:,1], yHat[srtInd])
+	ax.scatter(
+		xMat[:,1].flatten().A[0], yMat.T[:, 0].flatten().A[0], s=2, c='red'
+	)
+	plt.show()
+	
+def resError(yArr, yHatArr):
+	return ((yArr - yHatArr)**2).sum()
+	
+def uciTest():
+	abX, abY = loadDataSet('abalone.txt')
+	trainX = abX[0:99]
+	trainY = abY[0:99]
+	yHat01 = lwlrTest(trainX, trainX, trainY, 0.1)
+	yHat1  = lwlrTest(trainX, trainX, trainY, 1)
+	yHat10 = lwlrTest(trainX, trainX, trainY, 10)
+	e01 = resError(trainY, yHat01.T)
+	e1  = resError(trainY, yHat1.T)
+	e10 = resError(trainY, yHat10.T)
+	print 'e01 =', e01
+	print 'e1 =', e1
+	print 'e10 =', e10
+	testX = abX[100:199]
+	testY = abY[100:199]
+	yHat01 = lwlrTest(testX, trainX, trainY, 0.1)
+	yHat1  = lwlrTest(testX, trainX, trainY, 1)
+	yHat10 = lwlrTest(testX, trainX, trainY, 10)
+	e01 = resError(testY, yHat01.T)
+	e1  = resError(testY, yHat1.T)
+	e10 = resError(testY, yHat10.T)
+	print 'e01 =', e01
+	print 'e1 =', e1
+	print 'e10 =', e10
+	
+def ridgeRegress(xMat, yMat, lam=0.2):
+	xTx = xMat.T * xMat
+	denom = xTx  + eye(shape(xMat)[1])*lam
+	if linalg.det(denom) == 0.0:
+		print 'This matrix is singular, cannot do inverse'
+		return
+	ws = denom.T * (xMat.T * yMat)
+	return ws
+	
+def ridgeTest(xArr, yArr):
+	xMat = mat(xArr)
+	yMat = mat(yArr).T
+	ymean = mean(yMat, 0)
+	yMat = yMat - ymean
+	xMeans = mean(xMat, 0)
+	xVar = var(xMat, 0)
+	xMat = (xMat - xMeans) / xVar
+	numTestPts = 30
+	wMat = zeros((numTestPts, shape(xMat)[1]))
+	for i in range(numTestPts):
+		ws = ridgeRegress(xMat, yMat, exp(i-10))
+		wMat[i,:] = ws.T
+	return wMat
+	
+def testRidge():
+	xArr, yArr = loadDataSet('abalone.txt')
+	ridgeWeights = ridgeTest(xArr, yArr)
+	print ridgeWeights
+	fig = plt.figure()
+	ax = fig.add_subplot(111)
+	ax.plot(ridgeWeights)
+	plt.show()
+	
+def regularize(xMat):#regularize by columns
+    inMat = xMat.copy()
+    inMeans = mean(inMat,0)   #calc mean then subtract it off
+    inVar = var(inMat,0)      #calc variance of Xi then divide by it
+    inMat = (inMat - inMeans)/inVar
+    return inMat
+	
+def stageWise(xArr, yArr, eps=0.01, numIt=100):
+	xMat = mat(xArr)
+	yMat = mat(yArr).T
+	yMean = mean(yMat, 0)
+	yMat = yMat - yMean
+	xMat = regularize(xMat)
+	m, n = shape(xMat)
+	returnMat = zeros((numIt, n))
+	ws = zeros((n, 1))
+	wsTest = ws.copy()
+	wsMax = ws.copy()
+	for i in range(numIt):
+		# print ws.T
+		lowestError = inf
+		for j in range(n):
+			for sign in [-1, 1]:
+				wsTest = ws.copy()
+				wsTest[j] += eps*sign
+				yTest = xMat*wsTest
+				resE = resError(yMat.A, yTest.A)
+				if resE < lowestError:
+					lowestError = resE
+					wsMax = wsTest
+		ws = wsMax.copy()
+		returnMat[i, :] = ws.T
+	return returnMat
+	
+def testStageWise():
+	xArr, yArr = loadDataSet('abalone.txt')
+	weights = stageWise(xArr, yArr, 0.001, 5000)
+	print weights
+	xMat = mat(xArr)
+	yMat = mat(yArr).T
+	xMat = regularize(xMat)
+	yMean = mean(yMat, 0)
+	yMat = yMat - yMean
+	print standRegress(xMat, yMat.T)
+	
+if __name__ == '__main__':
+	# testStandRegress()
+	# xArr, yArr = loadDataSet('ex0.txt')
+	# ws = standRegress(xArr, yArr)
+	# xMat = mat(xArr)
+	# yMat = mat(yArr)
+	# yHat = xMat * ws
+	# print corrcoef(yHat.T, yMat)
+	# testLwlr()
+	# uciTest()
+	# testRidge()
+	testStageWise()
